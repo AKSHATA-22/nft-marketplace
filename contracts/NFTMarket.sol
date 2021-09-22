7860edaba9eb536566cc34febb38a2466b3259b1
AKSHATA-22 <akshatasangwai1234@gmail.com>
2021-09-22 16:49:03 +0530

    createMarketItem done (43)

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -10,5 +10,70 @@ contract NFTMarket is ReentrancyGuard{
     Counters.Counter private _itemIds;
     Counters.Counter private _itenSold;
 
-    
+    address payable owner;
+    uint256 listingPrice = 0.025 ether;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem{
+        uint ItemId;
+        address nftContract;
+        uint256 tokenId;
+        address payable seller;
+        address payable owner;
+        uint256 price;
+        bool sold;
+    }
+
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    event MarketItemCreated(
+        uint indexed itemId,
+        address indexed nftContract,
+        uint256 indexed tokenId,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    function getListingPrice() public view returns (uint256) {
+        return listingPrice;
+    }
+
+    function createMarketItem(
+    address nftContract,
+    uint256 tokenId,
+    uint256 price) public payable nonReentrant{
+        require(price > 0, "Price must be atleat 1 wei");
+        require(msg.value == listingPrice, "Price must be equal to listing price");
+
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();    
+
+        idToMarketItem[itemId] = MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+        emit MarketItemCreated(
+            itemId, 
+            nftContract, 
+            tokenId, 
+            msg.sender, 
+            address(0), 
+            price, 
+            false
+            );
+    }
+    // nonReentrant is called modifier
 }
