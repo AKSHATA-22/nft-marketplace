aa2340e0f545d8bf052c5b28af2d975ba46f3124
AKSHATA-22 <akshatasangwai1234@gmail.com>
2021-09-25 09:23:30 +0530

    testing worked

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,58 @@
+// require("@nomiclabs/hardhat-waffle");
+// const fs = require("fs")
+// const privateKey = fs.readFileSync(".secret").toString()
+// const projectId = "615b77a697d647e58cbd0696ad8a3b29"
+
+// module.exports = {
+//   networks: {
+//     hardhat: {
+//       chainId: 1337
+//     },
+//     mumbai: {
+//       url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+//       accounts: [privateKey]
+//     },
+//     mainnet: {
+//       url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+//       accounts: [privateKey]
+//     }
+//   },
+//   solidity: "0.8.4",
+// };
+
 require("@nomiclabs/hardhat-waffle");
-const fs = require("fs")
-const privateKey = fs.readFileSync(".secrect").toString()
-const projectId = "615b77a697d647e58cbd0696ad8a3b29"
+const fs = require('fs');
+// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337
     },
+    /*
     mumbai: {
-      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      // Infura
+      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      url: "https://rpc-mumbai.matic.today",
       accounts: [privateKey]
     },
-    mainnet: {
-      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+    matic: {
+      // Infura
+      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      url: "https://rpc-mainnet.maticvigil.com",
       accounts: [privateKey]
     }
+    */
   },
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
